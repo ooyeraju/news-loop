1ed48cd639a9d5e1ec5a0a47998b41383fa6356f
ooyeraju <rajnishkush2@gmail.com>
2020-04-14 03:53:35 +0530

    this is an update

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template
 import requests
 # from news import news_head
+
+
 app = Flask(__name__)
-
-
 
 @app.route('/')
 def index():
+    coun_code = 'in'
     url = ('http://newsapi.org/v2/top-headlines?'
-       'country=in&'
-       'apiKey=456146a9a724410ba0b89d244c709bc1')
+       'country={}&'
+       'apiKey=456146a9a724410ba0b89d244c709bc1').format(coun_code)
     top_head = requests.get(url)
     top_head_json = top_head.json()
 
