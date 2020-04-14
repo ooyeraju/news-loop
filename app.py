8b420254e6ec55e6d744f9d178fab9b953287d24
ooyeraju <rajnishkush2@gmail.com>
2020-04-14 13:15:17 +0530

    remove api key

--- app.py
+++ app.py
@@ -1,38 +1,36 @@
 from flask import Flask, render_template, request
 import requests
+import yaml
 # from news import news_head
 
 
 app = Flask(__name__)
+ak = yaml.load(open('ak.yaml'))
 
 @app.route('/')
 def index():
     coun_code = 'in'
     url = ('http://newsapi.org/v2/top-headlines?'
        'country={}&'
-       'apiKey=456146a9a724410ba0b89d244c709bc1').format(coun_code)
+       'apiKey={}').format(coun_code, ak['api_key'])
     top_head = requests.get(url)
     top_head_json = top_head.json()
     news_head = []
     news_link = []
     news_img = []
-
     for i in top_head_json['articles']:
             a = i['title']
             b = i['url']
             c = i['urlToImage']
             news_head.append(a)
             news_link.append(b)
-            news_img.append(c)
-            
+            news_img.append(c)    
     news_material = zip(news_head, news_link, news_img)
-    
     return render_template('index.html', news_material = news_material)
 
 @app.route('/input')
 def input():
     return render_template('input.html')
-
 
 
 @app.route('/search', methods=['POST', 'GET'])
@@ -41,8 +39,8 @@ def search():
         coun_code= request.form['coun_code']
         # print(coun_code)
         url = ('http://newsapi.org/v2/top-headlines?'
-        'country={}&'
-        'apiKey=456146a9a724410ba0b89d244c709bc1').format(coun_code)
+       'country={}&'
+       'apiKey={}').format(coun_code, ak['api_key'])
         top_head = requests.get(url)
         top_head_json = top_head.json()
         news_head = []
@@ -51,11 +49,8 @@ def search():
                 news_head.append(a)
         # print(url)
         # print(news_head)
-        
     return render_template('search.html', news_head=news_head, coun_code = coun_code )
     # return 'hi'
-
-
 
 
 
